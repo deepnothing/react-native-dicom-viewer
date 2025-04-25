c778ce5d350ee0c984c1677beb6b461b4fd16bf5
teoudovcic@gmail.com <teoudovcic@gmail.com>
2025-04-24 20:08:08 -0500

    add type defenitions

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,18 @@ import {
 } from 'react-native';
 
 type DicomViewerProps = ViewProps & {
+  /**
+   * The source URL or path to the DICOM file to be displayed.
+   */
   src?: string;
+
+  /**
+   * Callback triggered when the displayed frame changes.
+   * 
+   * @param event - Event sent from native code .
+   * @param event.nativeEvent.index - The index of the currently displayed frame (0-based).
+   * @param event.nativeEvent.total - The total number of frames in the DICOM file.
+   */
   onFrameChange?: (event: {
     nativeEvent: { index: number; total: number };
   }) => void;
